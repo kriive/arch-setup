fc77303a416b33ac02272ca19f65149fe81d0101
Manuel Romei <manuel.romei@fander.it>
2023-07-13 13:45:29 +0200

    Update configs and use proper paths

--- usr/local/bin/idle.sh
+++ usr/local/bin/idle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 swayidle \
-	timeout 600 '~/.config/sway/lock.sh' \
+	timeout 600 'chayang && /usr/local/bin/lock.sh' \
 	timeout 900 'swaymsg "output * dpms off"' \
 	resume 'swaymsg "output * dpms on"' \
-	before-sleep '~/.config/sway/lock.sh'
+	before-sleep '/usr/local/bin/lock.sh'
